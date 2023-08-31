e5aecbc04cc49fc7679e263cbb454874305d6b94
mt-git <prem.kumar@mindtickle.com>
2023-08-31 22:53:08 +0530

    feat: Lession 1 - React basic and without npm

---
+++ Lession 1/App.js
@@ -0,0 +1,8 @@
+const parent = React.createElement("h1", { className: "root-div" }, [
+  React.createElement("div", { className: "children1" }, "children1"),
+  React.createElement("div", { className: "children2" }, "children2"),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # React-EP1-Notes
+
+# - what is CDN, why, how works
+# what is cross origin in script tag
